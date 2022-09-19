0468cb76739edd9798984d26257452b7fc778e80
denis.rodriguez <denis.rodriguez@et.bo>
2022-09-19 17:31:51 -0400

    ADD EXAMPLE

--- src/config/config.js
+++ src/config/config.js
@@ -3,4 +3,62 @@ export const endpoint = `https://newsapi.org/v2/top-headlines`
 export const country = 'ar'
 
 // * FIRESTORE
-export const collectionNews = 'news' 
+export const collectionNews = 'news'
+/*
+
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import analytics from '@react-native-firebase/analytics';
+
+
+const LoginFunction = (props) => {
+
+    const [name, setName] = useState("")
+
+    //crecion
+    useEffect(() => {
+        console.log("Init App")
+    }, [])
+
+
+    // Update of state variable
+    useEffect(() => {
+        if (name != '')
+            console.log("Updated")
+    }, [name])
+
+    // Eliminacion
+    useEffect(() => {
+        return () => {
+            console.log("Component Deleted!")
+        }
+    }, [])
+
+    const handleButton = () => {
+        setName('Denis')
+    }
+
+    return (
+        <View>
+            <Text style={style.headerColor}>Componente de función: {name} {props.lastName}</Text>
+            <Button onPress={handleButton} title="Nombre Funcion" />
+            <Button onPress={async () => {
+                await analytics().logEvent('LOG_ET')
+                console.log("LOGEADO")
+            }
+            }
+                title="LOGGER" />
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    headerColor: {
+        color: 'black',
+        fontWeight: "bold"
+    }
+})
+
+export default LoginFunction
+
+*/
